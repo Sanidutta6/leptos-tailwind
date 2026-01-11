6a3ea79d8844641b168632b10f3e5dde0712d40f
Suny Dutta <sanidutta6@gmail.com>
2026-01-11 13:21:01 +0530

    Added sidebar trigger to trigger sidebar state.

--- src/components/ui/private_header.rs
+++ src/components/ui/private_header.rs
@@ -1,11 +1,14 @@
-use leptos::prelude::*;
 use crate::components::base::sidebar::SidebarTrigger;
+use leptos::prelude::*;
 
 #[component]
 pub fn PrivateHeader() -> impl IntoView {
     view! {
-        <header>
-            <SidebarTrigger />
+        <header class="border-b py-2">
+            <div class="flex items-center gap-2">
+                <SidebarTrigger />
+                <p class="text-2xl font-bold">"Brand"</p>
+            </div>
         </header>
     }
 }
