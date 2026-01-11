776c14fd68937308399b23632076a0c296af91a2
Suny Dutta <sanidutta6@gmail.com>
2026-01-11 20:30:02 +0530

    Added logout functionality and ui.

--- src/components/ui/private_header.rs
+++ src/components/ui/private_header.rs
@@ -1,14 +1,20 @@
-use crate::components::base::sidebar::SidebarTrigger;
-use leptos::prelude::*;
+use crate::components::base::{button::Button, sidebar::SidebarTrigger};
+use leptos::{prelude::*, server::codee::string::FromToStringCodec};
+use leptos_use::use_cookie;
 
 #[component]
 pub fn PrivateHeader() -> impl IntoView {
+    let (_, set_cookie) = use_cookie::<String, FromToStringCodec>("auth_token");
+
     view! {
-        <header class="border-b py-2">
+        <header class="border-b py-2 px-3 flex items-center justify-between">
             <div class="flex items-center gap-2">
                 <SidebarTrigger />
                 <p class="text-2xl font-bold">"Brand"</p>
             </div>
+            <Button on_click=move || {
+                set_cookie.set(None)
+            }>"Logout"</Button>
         </header>
     }
 }
